48b73f8734a687784db005dfcfe69ce0782475e2
Marina Emelianova <argenty@gmail.com>
2023-07-28 14:49:25 +0300

    fix main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,12 @@
 import style from './Main.module.css';
+import Layout from '../Layout';
 
 export const Main = () => {
-  return <div></div>
+  return <main className={style.main}>
+    <Layout>
+      <div className={style.gridContainer}>
+        
+      </div>
+    </Layout>
+  </main>
 }
