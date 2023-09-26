93451eab9460f92ea1cf2761c4a84b21a9921306
Marina Emelianova <argenty@gmail.com>
2023-09-26 13:11:20 +0300

    test2

--- src/components/Modal/Comments/Comments.jsx
+++ src/components/Modal/Comments/Comments.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable operator-linebreak */
 import style from './Comments.module.css';
 import PropTypes from 'prop-types';
@@ -39,28 +40,32 @@ import Dates from '../../Main/List/Post/Dates';
 };
 */
 
-export const Comments = ({comments}) => (
-  <ul className={style.list}>
-    {comments.length ? (
-      comments.map(
-        (item) =>
-          item.body && (
-            <li className={style.item} key={item.id}>
-              <Text As="h3" className={style.author} size={18} tsize={22}>
-                {item.author}
-              </Text>
-              <Text As="p" className={style.comment} size={14} tsize={18}>
-                {item.body.replaceAll(`&gt;`, ' ')}
-              </Text>
-              <Dates date={item.created} />
-            </li>
-          ),
-      )
-    ) : (
-      <p>No comments</p>
-    )}
-  </ul>
-);
+export const Comments = ({comments}) => {
+  return (
+    <ul className={style.list}>
+      {comments.length ? (
+        comments.map(item => {
+          return (
+            console.log(item) &&
+            item.body && (
+              <li className={style.item} key={item.id}>
+                <Text As="h3" className={style.author} size={18} tsize={22}>
+                  {item.author}
+                </Text>
+                <Text As="p" className={style.comment} size={14} tsize={18}>
+                  {item.body.replaceAll(`&gt;`, ' ')}
+                </Text>
+                <Dates date={item.created} />
+              </li>
+            )
+          );
+        })
+      ) : (
+        <p>No comments</p>
+      )}
+    </ul>
+  );
+};
 
 Comments.propTypes = {
   comments: PropTypes.array,
